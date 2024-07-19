6a5e407ec21603b1566b341666fa19991abf85a9
Cristhian Sevilla <crissev17@gmail.com>
2024-07-19 16:36:54 -0600

    add menu for mobile devices

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <v-app max-width="1200" class="mx-auto bg-grey-lighten-4">
+  <v-app>
     <!-- Barra de navegación -->
     <v-app-bar class="bg-white" elevation="0">
-      <!-- Botón de menú para dispositivos móviles -->
-      <v-app-bar-nav-icon @click="toggleDrawer" class="d-lg-none" />
-      <v-navigation-drawer v-model="drawer" app temporary class="d-lg-none">
-        <v-list>
-          <v-list-item :to="{ name: 'home' }" @click="closeDrawer">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>HomeQuest</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item :to="{ name: 'home' }" @click="closeDrawer">
-            <v-list-item-content>
-              <v-list-item-title>Catálogo</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item :to="{ name: 'login' }" @click="closeDrawer">
-            <v-list-item-content>
-              <v-list-item-title>Iniciar sesión</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <!-- Menú para dispositivos móviles -->
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ props }">
+          <v-btn
+            v-bind="props"
+            class="text-blue-darken-2 text-none text-h6 d-lg-none"
+            prepend-icon="mdi-home"
+          >
+            <template v-slot:prepend>
+              <v-icon color="grey-darken-2"></v-icon>
+            </template>
+            HomeQuest
+          </v-btn>
+        </template>
 
+        <v-card min-width="350">
+          <v-list>
+            <v-list-item @click="navigateTo('home')">Catálogo</v-list-item>
+            <v-divider></v-divider>
+            <v-list-item @click="navigateTo('login')"
+              >Iniciar sesión</v-list-item
+            >
+          </v-list>
+          <v-divider></v-divider>
+        </v-card>
+      </v-menu>
       <!-- Contenido de la barra de navegación para pantallas grandes -->
       <v-app-bar-title class="d-none d-lg-flex">
         <v-btn
@@ -40,9 +45,6 @@
           HomeQuest
         </v-btn>
       </v-app-bar-title>
-
-      <v-spacer />
-
       <template #append>
         <v-btn
           class="text-blue-darken-2 text-none text-body-1 d-none d-lg-flex"
@@ -64,34 +66,27 @@
         <RouterView />
       </v-container>
     </v-main>
-    <v-main>
-      <v-container>
-        <RouterView />
-      </v-container>
-    </v-main>
+
     <v-footer class="d-flex justify-center align-center bg-white" padless>
-      <span class="text-grey-darken-1 text-center"
-        >© {{ currentYear }} HomeQuest. Todos los derechos reservados.</span
-      >
+      <span class="text-grey-darken-1 text-center">
+        © {{ currentYear }} HomeQuest. Todos los derechos reservados.
+      </span>
     </v-footer>
   </v-app>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { RouterView } from "vue-router";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const menu = ref(false);
 
 // Obtener el año actual
 const currentYear = new Date().getFullYear();
-const drawer = ref(false); // Estado del menú de navegación
-
-// Función para alternar el estado del drawer
-const toggleDrawer = () => {
-  drawer.value = !drawer.value;
-};
-
-// Función para cerrar el drawer
-const closeDrawer = () => {
-  drawer.value = false;
+// Función para cerrar el menú y navegar
+const navigateTo = (routeName) => {
+  router.push({ name: routeName });
+  menu.value = false; // Cierra el menú después de la navegación
 };
 </script>
