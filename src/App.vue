db64087bc26e8d3c42d25c7a9507e656e275b40f
Cristhian Sevilla <crissev17@gmail.com>
2024-07-20 18:03:20 -0600

    standalone router view background video

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app class="bg-grey-lighten-4 position-relative">
+    <!-- Contenedor del video de fondo -->
+    <div
+      class="video-container position-fixed fill-height w-100 overflow-hidden top-0 left-0"
+    >
+      <video autoplay loop muted class="background-video w-100 h-100">
+        <source src="/video/login.mp4" type="video/mp4" />
+      </video>
+      <div
+        class="video-shadow top-0 left-0 w-100 h-100 position-absolute"
+      ></div>
+    </div>
+
     <!-- Barra de navegación -->
     <NavBar />
+
     <div
       class="d-flex justify-center align-center fill-height w-100 position-relative"
     >
       <div class="position-relative fill-height w-100 overflow-hidden">
-        <video
-          autoplay
-          loop
-          muted
-          class="background-video top-0 left-0 w-100 h-100 position-absolute"
-        >
-          <source src="/video/login.mp4" type="video/mp4" />
-        </video>
-        <div
-          class="video-shadow top-0 left-0 w-100 h-100 position-absolute"
-        ></div>
         <v-main>
           <v-container>
             <RouterView />
@@ -24,8 +26,9 @@
         </v-main>
       </div>
     </div>
+
     <v-footer
-      class="d-flex justify-center align-center bg-transparent abosolute bottom-0 left-0 w-100 position-absolute"
+      class="d-flex justify-center align-center bg-transparent absolute bottom-0 left-0 w-100"
     >
       <span class="text-white text-center">
         © {{ currentYear }} HomeQuest. Todos los derechos reservados.
@@ -45,7 +48,9 @@ const currentYear = new Date().getFullYear();
 .background-video {
   object-fit: cover;
 }
+
 .video-shadow {
   background: rgba(255, 255, 255, 0.1);
+  pointer-events: none;
 }
 </style>
