8305134824fac0095f617a8561765e68a9755370
Cristhian Sevilla <crissev17@gmail.com>
2024-07-24 19:13:20 -0600

    menu redesign

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,48 +1,12 @@
 <template>
-  <v-app-bar elevation="0" class="bg-light-green-darken-1">
+  <v-app-bar app elevation="0" class="bg-light-green-darken-1">
     <!-- Menú para dispositivos móviles -->
-    <v-menu
-      v-model="menu"
-      :close-on-content-click="false"
-      transition="scale-transition"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          v-bind="props"
-          class="text-h6 d-lg-none text-withe"
-          prepend-icon="mdi-home"
-        >
-          <template v-slot:prepend>
-            <v-icon color="withe"></v-icon>
-          </template>
-          HomeQuest
-        </v-btn>
-      </template>
-
-      <v-card min-width="350">
-        <v-list v-if="isAuth" class="bg-light-green-darken-1">
-          <v-list-item class="text-withe" @click="navigateTo('manage-houses')">
-            Administración
-          </v-list-item>
-          <v-divider></v-divider>
-          <v-list-item class="text-withe" @click="logout">
-            Cerrar sesión
-          </v-list-item>
-        </v-list>
-        <v-list v-else class="bg-light-green-darken-1">
-          <v-list-item class="text-withe" @click="navigateTo('home')">
-            Inicio
-          </v-list-item>
-          <v-divider></v-divider>
-          <v-list-item class="text-withe" @click="navigateTo('login')">
-            Iniciar sesión
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-      </v-card>
-    </v-menu>
-    <!-- Contenido de la barra de navegación para pantallas grandes -->
-    <v-app-bar-title class="d-none d-lg-flex">
+    <v-app-bar-nav-icon
+      class="d-flex d-md-none"
+      @click.stop="drawer = !drawer"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title class="d-flex d-md-none">HomeQuest</v-toolbar-title>
+    <v-app-bar-title class="d-none d-md-flex">
       <v-btn
         class="text-h5 text-withe"
         prepend-icon="mdi-home"
@@ -57,24 +21,29 @@
     <template #append>
       <div class="d-flex" v-if="isAuth">
         <v-btn
-          class="d-none d-lg-flex text-withe"
+          class="d-none d-md-flex text-withe"
+          append-icon="mdi-wrench"
           :to="{ name: 'manage-houses' }"
         >
           Administración
         </v-btn>
-        <v-btn class="d-none d-lg-flex text-withe" @click="logout">
+        <v-btn
+          append-icon="mdi-logout"
+          class="d-none d-md-flex text-withe"
+          @click="logout"
+        >
           Cerrar sesión
         </v-btn>
       </div>
       <div class="d-flex" v-else>
         <v-btn
-          class="d-none d-lg-flex text-withe text-h6"
+          class="d-none d-md-flex text-withe text-h6"
           :to="{ name: 'home' }"
         >
           Inicio
         </v-btn>
         <v-btn
-          class="d-none d-lg-flex text-withe text-h6"
+          class="d-none d-md-flex text-withe text-h6"
           :to="{ name: 'login' }"
         >
           Iniciar sesión
@@ -82,6 +51,42 @@
       </div>
     </template>
   </v-app-bar>
+
+  <v-navigation-drawer
+    app
+    v-model="drawer"
+    class="d-md-none bg-green-lighten-5"
+  >
+    <v-list class="d-md-none">
+      <v-list-item
+        prepend-icon="mdi-home"
+        title="Inicio"
+        @click="navigateTo('home')"
+      ></v-list-item>
+      <v-divider></v-divider>
+
+      <v-list-item
+        prepend-icon="mdi-wrench"
+        title="Administración"
+        @click="isAuth ? navigateTo('manage-houses') : navigateTo('login')"
+      ></v-list-item>
+    </v-list>
+    <template v-slot:append>
+      <div class="pa-4">
+        <v-btn
+          class="mt-3"
+          size="large"
+          color="light-green-darken-2"
+          variant="elevated"
+          block
+          :prepend-icon="isAuth ? 'mdi-logout' : 'mdi-login'"
+          @click="isAuth ? logout() : navigateTo('login')"
+        >
+          {{ isAuth ? "Cerrar sesión" : "Iniciar sesión" }}
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
 </template>
 
 <script setup>
@@ -93,15 +98,17 @@ import { storeToRefs } from "pinia";
 const auth = useAuthStore();
 const { isAuth } = storeToRefs(auth);
 const router = useRouter();
-const menu = ref(false);
+const drawer = ref(false);
 
 // Función para cerrar el menú y navegar
 const navigateTo = (routeName) => {
   router.push({ name: routeName });
-  menu.value = false; // Cierra el menú después de la navegación
+  drawer.value = false; // Cierra el menú después de la navegación
 };
 const logout = () => {
   auth.logout();
-  menu.value = false;
+  drawer.value = false;
 };
 </script>
+
+<style scoped></style>
