b0db1613ba9611fff015ad2acf0f9da9f3d2f0ab
Cristhian Sevilla <crissev17@gmail.com>
2024-07-25 21:26:27 -0600

    Format the property price

--- src/composables/useProperties.js
+++ src/composables/useProperties.js
@@ -1,3 +1,4 @@
+import { computed } from "vue";
 import { collection } from "firebase/firestore";
 import { useFirestore, useCollection } from "vuefire";
 
@@ -5,7 +6,16 @@ export default function usePropertiesStore() {
   const db = useFirestore();
   const propertiesCollection = useCollection(collection(db, "properties"));
 
+  const formatPrice = computed(() => {
+    return (price) =>
+      Number(price).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+  });
+
   return {
     propertiesCollection,
+    formatPrice,
   };
 }
--- src/views/admin/ManageHousesView.vue
+++ src/views/admin/ManageHousesView.vue
@@ -1,10 +1,36 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-card class="mx-auto mt-10">
+      <v-list>
+        <v-list-item
+          v-for="property in propertiesCollection"
+          :key="property.id"
+          border
+        >
+          <template #prepend>
+            <v-list-item-media :start="true">
+              <img
+                :src="property.image"
+                alt="Imagen de la propiedad"
+                width="200"
+              />
+            </v-list-item-media>
+          </template>
+          <v-list-item-title>{{ property.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{
+            formatPrice(property.price)
+          }}</v-list-item-subtitle>
+          <template #append>
+            <v-btn color="info" flat class="mr-2"> Editar </v-btn>
+            <v-btn color="error" flat> Eliminar </v-btn>
+          </template>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-container>
 </template>
 <script setup>
 import useProperties from "@/composables/useProperties";
 
-const { propertiesCollection } = useProperties();
-
-console.log(propertiesCollection);
+const { propertiesCollection, formatPrice } = useProperties();
 </script>
